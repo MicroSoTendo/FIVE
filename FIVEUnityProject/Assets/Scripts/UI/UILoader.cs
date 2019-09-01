8c3d5dde12c848bfbb2a80e48330d37fe06c2e60
Gen <xgbarry@gmail.com>
2019-08-31 21:46:27 -0400

    Multithreaded event system fixed.

--- FIVEUnityProject/Assets/Scripts/UI/UILoader.cs
+++ FIVEUnityProject/Assets/Scripts/UI/UILoader.cs
@@ -1,14 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Assets.Script.EventSystem;
+using Assets.Scripts.EventSystem;
 
 public class UILoader : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        EventSystem.RunOnce();
     }
 
     // Update is called once per frame
