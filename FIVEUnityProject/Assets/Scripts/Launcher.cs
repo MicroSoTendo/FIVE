1cf108ccf391104eac44dd34f8a0a0674eba65d9
Gen <xgbarry@gmail.com>
2019-09-03 16:04:38 -0400

    UI System prototype.

---
+++ FIVEUnityProject/Assets/Scripts/Launcher.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+using Assets.Scripts.EventSystem;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class Launcher : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public GameObject groundPrefab;
+
+        private Canvas canvas;
+        private CameraController cameraController;
+        private bool instantiated;
+        void Start()
+        {
+            canvas = GetComponentInChildren<Canvas>();
+            cameraController = GameObject.Find("Main Camera").GetComponent<CameraController>();
+            cameraController.enabled = false;
+            instantiated = false;
+            EventSystem.EventSystem.Subscribe(EventTypes.OnButtonClicked, OnButtonClicked);
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            var go = sender as Button;
+            if (go == null) return;
+            switch (go.name)
+            {
+                case "ExitButton":
+#if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+#endif
+                    Application.Quit(0);
+                    break;
+                case "StartButton":
+                    if (!instantiated)
+                    {
+                        Instantiate(groundPrefab);
+                        instantiated = true;
+                        go.GetComponentInChildren<Text>().text = "Resume";
+                    }
+
+                    canvas.gameObject.SetActive(false);
+                    cameraController.enabled = true;
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                canvas.gameObject.SetActive(!canvas.gameObject.activeSelf);
+                cameraController.enabled = !cameraController.enabled;
+            }
+        }
+    }
+}
--- FIVEUnityProject/Assets/Scripts/UI/UILoader.cs
+++ FIVEUnityProject/Assets/Scripts/UI/UILoader.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts.EventSystem;
+using UnityEngine.UI;
 
 public class UILoader : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Canvas canvas;
+    public GameObject menuButtonPrefab;
     void Start()
     {
+        canvas = GetComponentInChildren<Canvas>();
+        InstantiateNewButton("Start", new Vector3(0,40,0));
+        InstantiateNewButton("Continue", new Vector3(0,0,0));
+        InstantiateNewButton("Setting", new Vector3(0,-40,0));
+        InstantiateNewButton("Exit", new Vector3(0,-80,0));
     }
+
+    private void InstantiateNewButton(string displayName, Vector3 position)
+    {
+        var buttonGameObject = Instantiate(menuButtonPrefab, canvas.transform);
+        buttonGameObject.transform.localPosition = position;
+        buttonGameObject.name = displayName + "Button";
+        var button = buttonGameObject.GetComponent<Button>();
+        button.GetComponentInChildren<Text>().text = displayName;
+        button.onClick.AddListener(delegate { EventSystem.RaiseEvent(EventTypes.OnButtonClicked, button, EventArgs.Empty); });
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
