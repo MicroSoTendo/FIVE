3cb56317721ad51877cdea0c581bec6449ea4609
Yu Wei <winly8866@gmail.com>
2019-09-04 16:59:21 -0400

    Camera

---
+++ FIVEUnityProject/Assets/Scripts/Camera/CameraTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTest : MonoBehaviour
+{
+    private Color col;
+    private void Start()
+    {
+        col = this.GetComponent<Renderer>().material.color;
+    }
+    void OnMouseOver()
+    {
+        this.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        this.GetComponent<Renderer>().material.color = col;
+    }
+}
