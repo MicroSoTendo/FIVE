1fe72128724c5200e2bc022af35dde2f1b385f76
Wally Yang <wallyyang28@gmail.com>
2019-09-05 12:02:23 -0400

    fix: remove obsolete code

--- FIVEUnityProject/Assets/Scripts/CameraController.cs
+++ FIVEUnityProject/Assets/Scripts/CameraController.cs
@@ -9,32 +9,32 @@ namespace Assets.Scripts
 {
     internal class CameraController : MonoBehaviour
     {
-        private void Update()
-        {
-            if (Input.GetKey(KeyCode.W))
-            {
-                var d = transform.forward.normalized;
-                d.y = 0f;
-                d *= 2f;
-                d = transform.InverseTransformDirection(d);
-                transform.Translate(d);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                var d = transform.forward.normalized;
-                d.y = 0f;
-                d *= -2f;
-                d = transform.InverseTransformDirection(d);
-                transform.Translate(d);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                transform.Rotate(0f, -2f, 0f, Space.World);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Rotate(0f, 2f, 0f, Space.World);
-            }
-        }
+        //private void Update()
+        //{
+        //    if (Input.GetKey(KeyCode.W))
+        //    {
+        //        var d = transform.forward.normalized;
+        //        d.y = 0f;
+        //        d *= 2f;
+        //        d = transform.InverseTransformDirection(d);
+        //        transform.Translate(d);
+        //    }
+        //    if (Input.GetKey(KeyCode.S))
+        //    {
+        //        var d = transform.forward.normalized;
+        //        d.y = 0f;
+        //        d *= -2f;
+        //        d = transform.InverseTransformDirection(d);
+        //        transform.Translate(d);
+        //    }
+        //    if (Input.GetKey(KeyCode.A))
+        //    {
+        //        transform.Rotate(0f, -2f, 0f, Space.World);
+        //    }
+        //    if (Input.GetKey(KeyCode.D))
+        //    {
+        //        transform.Rotate(0f, 2f, 0f, Space.World);
+        //    }
+        //}
     }
 }
