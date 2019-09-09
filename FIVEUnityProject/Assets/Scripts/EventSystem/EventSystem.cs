503d8c0fba72a42b08684457c66c08fcd4ba46ef
Gen <xgbarry@gmail.com>
2019-09-09 14:37:31 -0400

    New event system.

--- FIVEUnityProject/Assets/Scripts/EventSystem/EventSystem.cs
+++ FIVEUnityProject/Assets/Scripts/EventSystem/EventSystem.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -10,46 +12,20 @@ namespace Assets.Scripts.EventSystem
 {
     public sealed class EventSystem
     {
-        public enum RunningMode
-        {
-            Inactive,
-            Coroutine,
-            Update,
-            Asynchronous
-        }
-
-        public struct Recipient
-        {
-            public EventTypes EventTypes { get; }
-            public EventHandler Handler { get; }
-
-            public Recipient(EventTypes eventTypes, EventHandler handler)
-            {
-                Handler = handler;
-                EventTypes = eventTypes;
-            }
-        }
-
         private static EventSystem Instance { get; } = new EventSystem();
-        private readonly EventHandler[] listOfHandlers;
-        private readonly ConcurrentQueue<Action> requestsQueue;
+        private readonly ConcurrentDictionary<Type, HandlerList<HandlerNode>> typedHandlerNodes;
+        private readonly ConcurrentQueue<Action> scheduledAsync = new ConcurrentQueue<Action>();
+        private readonly ConcurrentQueue<Action> scheduledMainThread = new ConcurrentQueue<Action>();
         private readonly int asynchronousTimeOut; //ms
         private readonly MainThreadDispatcher dispatcher;
-        private RunningMode runningMode;
         private bool initialized;
         private Coroutine coroutine;
-
+        private Task asyncTask;
         private EventSystem()
         {
-            listOfHandlers = new EventHandler[Enum.GetNames(typeof(EventTypes)).Length];
-            for (var i = 0; i < listOfHandlers.Length; i++)
-            {
-                listOfHandlers[i] += (s, e) => { };
-            }
-            requestsQueue = new ConcurrentQueue<Action>();
             var mainThreadDispatcther = new GameObject(nameof(MainThreadDispatcher));
             dispatcher = mainThreadDispatcther.AddComponent<MainThreadDispatcher>();
-            runningMode = RunningMode.Inactive;
+            typedHandlerNodes = new ConcurrentDictionary<Type, HandlerList<HandlerNode>>();
             asynchronousTimeOut = 16;
         }
 
@@ -57,122 +33,93 @@ namespace Assets.Scripts.EventSystem
         private static void Initialize()
         {
             if (Instance.initialized) return;
-            SetRunningMode(RunningMode.Coroutine); //Default mode = Coroutine
+            var types = from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                        from type in assembly.GetTypes()
+                        where typeof(IEventType).IsAssignableFrom(type)
+                        select type;
+            foreach (var type in types)
+            {
+                Instance.typedHandlerNodes.TryAdd(type, new HandlerList<HandlerNode>());
+            }
+            Instance.coroutine = Instance.dispatcher.StartCoroutine(Instance.MainThreadConsumer());
+            Instance.asyncTask = Task.Run(Instance.AsyncConsumer);
             Instance.initialized = true;
         }
 
-        public static void SetRunningMode(RunningMode newMode)
-        {
-            var oldMode = Instance.runningMode;
-            if (oldMode == newMode) return;
-            if (oldMode == RunningMode.Coroutine) Instance.dispatcher.StopCoroutine(Instance.coroutine);
-            else Instance.dispatcher.OnUpdate = () => { };
-            switch (newMode)
-            {
-                case RunningMode.Coroutine:
-                    Instance.coroutine = Instance.dispatcher.StartCoroutine(Instance.EventSystemCoroutine());
-                    break;
-                case RunningMode.Asynchronous:
-                    Instance.dispatcher.OnUpdate = async () => { await Instance.RunAsync(); };
-                    break;
-                case RunningMode.Update:
-                    Instance.dispatcher.OnUpdate = Instance.RunOnce;
-                    break;
-                case RunningMode.Inactive:
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(newMode), newMode, null);
-            }
-            Instance.runningMode = newMode;
-        }
-
-        public static Recipient Subscribe(EventTypes eventTypes, EventHandler handler, bool requiresMainThread = true)
-        {
-            Assert.IsTrue(Instance.initialized);
-            switch (Instance.runningMode)
-            {
-                case RunningMode.Inactive:
-                case RunningMode.Coroutine:
-                case RunningMode.Update:
-                case RunningMode.Asynchronous when !requiresMainThread:
-                    Instance.listOfHandlers[(uint)eventTypes] += handler;
-                    return new Recipient(eventTypes, handler);
-                case RunningMode.Asynchronous:
-                    void WrappedHandler(object s, EventArgs a)
-                    {
-                        MainThreadDispatcher.Schedule(delegate { handler(s, a); });
-                    }
-                    Instance.listOfHandlers[(uint)eventTypes] += WrappedHandler;
-                    return new Recipient(eventTypes, WrappedHandler);
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
-        }
-
-        public static Recipient Subscribe<T>(EventTypes eventTypes, EventHandler handler, bool requiresMainThread = true)
-        {
-            void DetermineType(object sender, EventArgs eventArgs)
-            {
-                if (sender is T) handler?.Invoke(sender, eventArgs);
-            }
-            return Subscribe(eventTypes, DetermineType, requiresMainThread);
-        }
-
-        public static void Unsubscribe(Recipient recipient)
-        {
-            Assert.IsTrue(Instance.initialized);
-            Instance.listOfHandlers[(uint)recipient.EventTypes] -= recipient.Handler;
-        }
-
-        public static EventHandler GetHandlers(EventTypes eventTypes)
-        {
-            Assert.IsTrue(Instance.initialized);
-            return Instance.listOfHandlers[(int)eventTypes];
-        }
-
-        public static void RaiseEvent(EventTypes eventTypes, object sender, EventArgs eventArgs)
-        {
-            Assert.IsTrue(Instance.initialized);
-            Debug.Log(nameof(RaiseEvent) + eventTypes + sender);
-            Instance.requestsQueue.Enqueue(delegate { Instance.listOfHandlers[(uint) eventTypes](sender, eventArgs); });
-        }
-
-        private void RunOnce()
-        {
-            while (requestsQueue.Count > 0)
-            {
-                requestsQueue.TryDequeue(out var action);
-                action.Invoke();
-            }
-        }
-
-        private IEnumerator EventSystemCoroutine()
+        private IEnumerator MainThreadConsumer()
         {
             while (true)
             {
-                RunOnce();
+                while (scheduledMainThread.Count > 0)
+                {
+                    float countDown = 1f;
+                    while (countDown > 0)
+                    {
+                        if (scheduledMainThread.TryDequeue(out var action))
+                            action();
+                        yield return null;
+                        countDown -= Time.deltaTime;
+                    }
+                }
                 yield return null;
             }
         }
 
-        private async Task RunAsync()
+        private async Task AsyncConsumer()
         {
-            var tasks = new Task[requestsQueue.Count];
-            var tokenSources = new CancellationTokenSource[requestsQueue.Count];
-            var i = 0;
-            while (requestsQueue.Count > 0)
+            Thread.CurrentThread.Name = nameof(AsyncConsumer);
+            while (true)
             {
-                requestsQueue.TryDequeue(out var action);
-                var tokenSource = new CancellationTokenSource(asynchronousTimeOut);
-                tokenSources[i] = tokenSource;
-                tasks[i++] = Task.Run(action, tokenSource.Token);
-            }
-            await Task.WhenAll(tasks);
-            foreach (var cancellationTokenSource in tokenSources)
-            {
-                cancellationTokenSource.Dispose();
+                while (scheduledAsync.Count > 0)
+                {
+                    scheduledAsync.TryDequeue(out var action);
+                    await Task.Run(action, new CancellationTokenSource(asynchronousTimeOut).Token);
+                }
+                Thread.Sleep(1);
             }
         }
+
+        public static void Subscribe<T>(EventHandler handler, bool requiresMainThread = true) where T : IEventType
+        {
+            Assert.IsTrue(Instance.initialized);
+            Instance.typedHandlerNodes[typeof(T)].Add(new HandlerNode(handler, requiresMainThread));
+        }
+
+        public static void Unsubscribe<T>(EventHandler handler, bool requiredMain)
+        {
+            Assert.IsTrue(Instance.initialized);
+            Instance.typedHandlerNodes[typeof(T)].Remove(new HandlerNode(handler, requiredMain));
+        }
+
+        public static async Task RaiseEventAsync<T>(object sender, EventArgs args)
+        {
+            await Task.Run(() =>
+            {
+                foreach (var (requiresMain, handlerNodes) in Instance.typedHandlerNodes[typeof(T)])
+                {
+                    var queue = requiresMain ? Instance.scheduledMainThread : Instance.scheduledAsync;
+                    foreach (var handlerNode in handlerNodes)
+                    {
+                        queue.Enqueue(delegate { handlerNode.Handler(sender, args); });
+                    }
+                }
+            });
+        }
+
+        public static void RaiseEvent<T>(object sender, EventArgs args)
+        {
+            foreach (var (requiresMain, handlerNodes) in Instance.typedHandlerNodes[typeof(T)])
+            {
+                var queue = requiresMain ? Instance.scheduledMainThread : Instance.scheduledAsync;
+                foreach (var handlerNode in handlerNodes)
+                {
+                    queue.Enqueue(delegate { handlerNode.Handler(sender, args); });
+                }
+            }
+        }
+
+
+
 
     }
 
