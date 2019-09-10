956224d6c46488c46c9e925729639b809beeb668
Gen <xgbarry@gmail.com>
2019-09-09 21:39:52 -0400

    updated.

--- FIVEUnityProject/Assets/Scripts/EventSystem/EventTypes/LauncherEvents.cs
+++ FIVEUnityProject/Assets/Scripts/EventSystem/EventTypes/LauncherEvents.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace FIVE.EventSystem.EventTypes
 {
-    public delegate void LauncherEventHandler(Launcher launcher, EventArgs args);
+    public delegate void LauncherEventHandler(MainLoader launcher, EventArgs args);
     public class OnLauncherAwake : IEventType { }
     public class OnLauncherStart : IEventType { }
     public class OnLauncherDestroyed : IEventType { }
