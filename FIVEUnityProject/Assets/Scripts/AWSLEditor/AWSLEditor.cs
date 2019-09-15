e96707579f836825820f00c315d6d28583b95e67
Laurence <liuxy6@gmail.com>
2019-09-15 19:09:44 +0100

    Add AWSLEditor

---
+++ FIVEUnityProject/Assets/Scripts/AWSLEditor/AWSLEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ASWLEditor : MonoBehaviour
+{
+    private string code = "";
+
+    private void Start()
+    {
+        enabled = false;
+    }
+
+    private void OnGUI()
+    {
+        Rect r = Screen.safeArea;
+        float w = r.width, h = r.height;
+        code = GUI.TextArea(new Rect(20, 40, w - 40, h - 80), code, int.MaxValue);
+    }
+}
