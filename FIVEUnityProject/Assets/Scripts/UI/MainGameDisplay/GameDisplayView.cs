fb34787db985be66d3715dae5d0e3ba69fca93c9
Yu Wei <winly8866@gmail.com>
2019-09-15 14:54:53 -0400

    added UI

---
+++ FIVEUnityProject/Assets/Scripts/UI/MainGameDisplay/GameDisplayView.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace FIVE.UI.MainGameDisplay
+{
+    public class GameDisplayView : View<GameDisplayView, GameDisplayViewModel>
+    {
+        public GameDisplayView()
+        {
+            Background = AddUIElement<Image>(nameof(Background));
+            PlayerButton = AddUIElement<Button>(nameof(PlayerButton));
+            Energy = AddUIElement<Button>(nameof(Energy));
+            Inventory = AddUIElement<Button>(nameof(Inventory));
+            Option = AddUIElement<Button>(nameof(Option));
+            //TestInputField = AddUIElement<InputField>(nameof(TestInputField));
+        }
+        public Image Background { get; }
+        public Button PlayerButton { get; }
+        public Button Energy { get; }
+        public Button Inventory { get; }
+        public Button Option { get; }
+        //public InputField TestInputField { get; }
+    }
+}
---
+++ FIVEUnityProject/Assets/Scripts/UI/OptionMenu/OptionMenuViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using UnityEngine;
+
+namespace FIVE.UI.OptionMenu
+{
+    public class OptionMenuViewModel : ViewModel<OptionMenuView, OptionMenuViewModel>
+    {
+
+        public string TestInputFieldText { get; set; }
+        public OptionMenuViewModel() : base()
+        {
+
+            binder.Bind(view => view.LoadButton.onClick).
+            To(viewModel => viewModel.OnLoadButtonClicked);
+            
+            binder.Bind(view => view.SaveButton.onClick).
+            To(viewModel => viewModel.OnSaveButtonClicked);
+            
+            binder.Bind(view => view.GameOptionButton.onClick).
+            To(viewModel => viewModel.OnGameOptionButtonClicked);
+            
+            binder.Bind(view => view.ExitGameButton.onClick).
+            To(viewModel => viewModel.OnExitButtonClicked);
+
+            binder.Bind(view => view.VideoOptionButton.onClick).
+            To(viewModel => viewModel.OnVideoOptionButtonClicked);
+
+            binder.Bind(view => view.SoundOptionButton.onClick).
+            To(viewModel => viewModel.OnSoundOptionButtonClicked);
+
+            binder.Bind(view=>view.ResumeButton.onClick).
+            To(viewModel=> viewModel.OnResumeButtonClicked);
+        }
+
+        private void OnLoadButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Debug.Log(nameof(OnLoadButtonClicked));
+        }
+        private void OnSaveButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Debug.Log(nameof(OnSaveButtonClicked));
+        }
+        private void OnGameOptionButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Debug.Log(nameof(OnGameOptionButtonClicked));
+        }
+        private void OnExitButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Debug.Log(nameof(OnExitButtonClicked));
+        }
+        private void OnResumeButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Debug.Log(nameof(OnResumeButtonClicked));
+        }
+        private void OnVideoOptionButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Debug.Log(nameof(OnVideoOptionButtonClicked));
+        }
+        private void OnSoundOptionButtonClicked(object sender, EventArgs eventArgs)
+        {
+            Debug.Log(nameof(OnSoundOptionButtonClicked));
+        }
+
+    }
+}
