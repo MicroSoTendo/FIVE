57fed09a5a15532d96fc689a57ee046d6ef0cd40
Laurence <liuxy6@gmail.com>
2019-09-15 19:46:26 +0100

    Update AWSLEditor

--- FIVEUnityProject/Assets/Scripts/AWSLEditor/AWSLEditor.cs
+++ FIVEUnityProject/Assets/Scripts/AWSLEditor/AWSLEditor.cs
@@ -2,17 +2,30 @@
 
 public class AWSLEditor : MonoBehaviour
 {
-    private string code = "";
+    public string code = "";
+
+    public Texture2D EditorBackground;
+    public Color EditorTextColor;
+
+    private GUIStyle style;
 
     private void Start()
     {
         enabled = false;
+        style = new GUIStyle
+        {
+            font = Font.CreateDynamicFontFromOSFont("Courier New", 16),
+            fontSize = 16,
+            wordWrap = true,
+        };
+        style.normal.textColor = EditorTextColor;
+        style.normal.background = EditorBackground;
     }
 
     private void OnGUI()
     {
         Rect r = Screen.safeArea;
         float w = r.width, h = r.height;
-        code = GUI.TextArea(new Rect(20, 40, w - 40, h - 80), code, int.MaxValue);
+        code = GUI.TextArea(new Rect(20, 40, w - 40, h - 80), code, int.MaxValue, style);
     }
 }
