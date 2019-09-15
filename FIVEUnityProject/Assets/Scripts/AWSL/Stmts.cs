baf55003954467c98f7af9f48bb3c483ffd66634
Wally Yang <wallyyang28@gmail.com>
2019-09-15 14:50:38 -0400

    feat: added AWSL framework

---
+++ FIVEUnityProject/Assets/Scripts/AWSL/Stmts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIVE
+{
+    internal class Stmts
+    {
+        internal Stmts()
+        {
+            stmt = new Stmt();
+        }
+
+        internal Stmt stmt;
+        internal Stmts stmts;
+    }
+}
