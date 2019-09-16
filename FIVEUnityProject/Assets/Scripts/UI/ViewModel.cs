3a4bf54f825a9a421587c83088a929a22e1b6e1f
Gen <xgbarry@gmail.com>
2019-09-15 21:27:59 -0400

    fixed derived type property access.

--- FIVEUnityProject/Assets/Scripts/UI/ViewModel.cs
+++ FIVEUnityProject/Assets/Scripts/UI/ViewModel.cs
@@ -2,22 +2,22 @@ namespace FIVE.UI
 {
     public abstract class ViewModel
     {
-        protected View view;
+        protected View View { get; set;}
         public void SetActive(bool value)
         {
-            view.ViewCanvas.gameObject.SetActive(value);
+            View.ViewCanvas.gameObject.SetActive(value);
         }
     }
     public abstract class ViewModel<TView, TViewModel> : ViewModel
         where TView : View<TView, TViewModel>, new()
         where TViewModel : ViewModel<TView, TViewModel>
     {
-        protected new TView view;
+        protected new TView View { get => base.View as TView; set => base.View = value; }
         protected Binder<TView, TViewModel> binder;
         protected ViewModel()
         {
-            view = View<TView, TViewModel>.Create<TView>();
-            binder = new Binder<TView, TViewModel>(view, this);
+            View = View<TView, TViewModel>.Create<TView>();
+            binder = new Binder<TView, TViewModel>(View, this);
         }
 
     }
