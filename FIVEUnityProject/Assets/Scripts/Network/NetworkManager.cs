71712d674553d4e522a349873deb84900978513a
Gen <xgbarry@gmail.com>
2019-09-16 18:17:47 -0400

    Network prototype.

---
+++ FIVEUnityProject/Assets/Scripts/Network/NetworkManager.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using FIVE.EventSystem;
+using FIVE.UI.StartupMenu;
+using Photon.Pun;
+using UnityEngine;
+using Photon.Realtime;
+
+namespace FIVE.Network
+{
+    public class NetworkManager : MonoBehaviourPunCallbacks
+    {
+        private LobbyInfoModel lobbyInfoModel;
+        void Start()
+        {
+            EventManager.Subscribe<OnMultiPlayersButtonClicked>(Initialize);
+            lobbyInfoModel = new LobbyInfoModel();
+        }
+
+        void Initialize(object sender, EventArgs args)
+        {
+            Debug.Log($"Initialize");
+            
+            var connectingResult = PhotonNetwork.ConnectUsingSettings();
+            if (connectingResult)
+            {
+                Debug.Log("connecting");
+            }
+            else
+            {
+                Debug.Log("fail to connect");
+            }
+        }
+
+        public override void OnConnected()
+        {
+            Debug.Log("Connected!");
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("OnConnectedToMaster() was called by PUN.");
+            bool result = PhotonNetwork.JoinLobby();
+            // PhotonNetwork.JoinRandomRoom();
+        }
+
+        public override void OnRoomListUpdate(List<RoomInfo> roomList)
+        {
+            lobbyInfoModel.RoomsList.Clear();
+            roomList.ForEach(o => lobbyInfoModel.RoomsList.Add(o));
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
