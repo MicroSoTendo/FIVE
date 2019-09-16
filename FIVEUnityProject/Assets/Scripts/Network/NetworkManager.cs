5ae2e2ea1df6a7b4bf35743747c5abfbd4a4b023
Gen <xgbarry@gmail.com>
2019-09-16 18:17:47 -0400

    Network prototype.

--- FIVEUnityProject/Assets/Scripts/Network/NetworkManager.cs
+++ FIVEUnityProject/Assets/Scripts/Network/NetworkManager.cs
@@ -1,61 +1,48 @@
 ﻿
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using FIVE.EventSystem;
-using FIVE.UI;
 using FIVE.UI.StartupMenu;
 using Photon.Pun;
 using UnityEngine;
-using System.Collections.Specialized;
-using System.ComponentModel;
 using Photon.Realtime;
 
 namespace FIVE.Network
 {
-    public class LobbyInfoModel : Observable
-    {
-        public ObservableCollection<RoomInfo> RoomsList { get; }
-
-        public LobbyInfoModel()
-        {
-            RoomsList = new ObservableCollection<RoomInfo>();
-            RoomsList.CollectionChanged += OnRoomsListChanged;
-        }
-
-        private void OnRoomsListChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            OnPropertyChange(new PropertyChangedEventArgs(nameof(RoomsList)));
-        }
-
-    }
-
     public class NetworkManager : MonoBehaviourPunCallbacks
     {
         private LobbyInfoModel lobbyInfoModel;
         void Start()
         {
-            PhotonNetwork.ConnectUsingSettings();
             EventManager.Subscribe<OnMultiPlayersButtonClicked>(Initialize);
+            lobbyInfoModel = new LobbyInfoModel();
         }
 
         void Initialize(object sender, EventArgs args)
         {
-            bool result = PhotonNetwork.JoinLobby();
-            if (result)
+            Debug.Log($"Initialize");
+            
+            var connectingResult = PhotonNetwork.ConnectUsingSettings();
+            if (connectingResult)
             {
-                Debug.Log($"{nameof(PhotonNetwork.JoinLobby)} Success");
+                Debug.Log("connecting");
             }
             else
             {
-                Debug.Log($"{nameof(PhotonNetwork.JoinLobby)} Failed");
+                Debug.Log("fail to connect");
             }
-
         }
+
+        public override void OnConnected()
+        {
+            Debug.Log("Connected!");
+        }
+
         public override void OnConnectedToMaster()
         {
             Debug.Log("OnConnectedToMaster() was called by PUN.");
-            PhotonNetwork.JoinRandomRoom();
+            bool result = PhotonNetwork.JoinLobby();
+            // PhotonNetwork.JoinRandomRoom();
         }
 
         public override void OnRoomListUpdate(List<RoomInfo> roomList)
@@ -64,7 +51,6 @@ namespace FIVE.Network
             roomList.ForEach(o => lobbyInfoModel.RoomsList.Add(o));
         }
 
-        // Update is called once per frame
         void Update()
         {
 
