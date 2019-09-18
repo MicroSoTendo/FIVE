451b82594d22623659beb3d03c226e502f664df3
Gen <xgbarry@gmail.com>
2019-09-18 04:09:19 -0400

    Minor fix.

--- FIVEUnityProject/Assets/Scripts/Network/MultiplayersTest.cs
+++ FIVEUnityProject/Assets/Scripts/Network/MultiplayersTest.cs
@@ -13,7 +13,12 @@ namespace FIVE.Network
         {
             Player = PhotonNetwork.Instantiate("EntityPrefabs/robotSphere", new Vector3(3, 0, 3), Quaternion.identity);
             photonView = Player.GetComponent<PhotonView>();
-            
+            photonView.ObservedComponents = new List<Component>{
+                Player.GetComponent<PhotonAnimatorView>(),
+                Player.GetComponent<PhotonTransformView>(),
+            };
+            photonView.Synchronization = ViewSynchronization.ReliableDeltaCompressed;
+
 
         }
 
