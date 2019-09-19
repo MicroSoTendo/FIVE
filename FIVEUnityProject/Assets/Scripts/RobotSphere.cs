7ac3d9c6090a38118f5a05c75c0930f3dc0dfb68
Gen <xgbarry@gmail.com>
2019-09-19 02:56:56 -0400

    Try2.

--- FIVEUnityProject/Assets/Scripts/RobotSphere.cs
+++ FIVEUnityProject/Assets/Scripts/RobotSphere.cs
@@ -34,6 +34,7 @@ namespace FIVE
         private void Awake()
         {
             //TODO: Check if camera is really "mine" in multiple players
+            if (photonView.IsMine == false && PhotonNetwork.IsConnected) return;
             fpsCamera = Instantiate(CameraPrefab);
             Transform eye = transform.GetChild(0).GetChild(1); // HACK
             fpsCamera.transform.parent = eye;
@@ -41,15 +42,19 @@ namespace FIVE
             //fpsCamera.transform.rotation = eye.rotation;
             fpsCamera.transform.localPosition = new Vector3(0, 0, 0);
             fpsCamera.transform.localRotation = Quaternion.Euler(0, 0, 0);
-            this.RaiseEvent<OnCameraCreated>(new OnCameraCreatedArgs { Id = "Robot" + this.GetInstanceID(), Camera = fpsCamera });
+            this.RaiseEvent<OnCameraCreated>(new OnCameraCreatedArgs
+            {
+                Id = "Robot" + this.GetInstanceID(),
+                Camera = fpsCamera
+            });
 
-            //CameraManager.AddBinding(fpsCamera, eye);
+            CameraManager.AddBinding(fpsCamera, eye);
 
-            //Camera camera2 = Instantiate(CameraPrefab);
-            //camera2.transform.parent = transform;
-            //camera2.transform.localPosition = new Vector3(0, 2, 0);
-            //camera2.transform.localRotation = Quaternion.Euler(90, 0, 0);
-            //this.RaiseEvent<OnCameraCreated>(new OnCameraCreatedArgs { Id = "Robot" + this.GetInstanceID() + " Camera 2", Camera = camera2 });
+            Camera camera2 = Instantiate(CameraPrefab);
+            camera2.transform.parent = transform;
+            camera2.transform.localPosition = new Vector3(0, 2, 0);
+            camera2.transform.localRotation = Quaternion.Euler(90, 0, 0);
+            this.RaiseEvent<OnCameraCreated>(new OnCameraCreatedArgs { Id = "Robot" + this.GetInstanceID() + " Camera 2", Camera = camera2 });
 
 
 
