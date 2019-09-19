184ecd1701240c1255c327d9abd49cd8aa8f66ff
Gen <xgbarry@gmail.com>
2019-09-19 13:09:17 -0400

    Added AvoidDestroy for scene onload.

---
+++ FIVEUnityProject/Assets/Scripts/AvoidDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FIVE
+{
+    public class AvoidDestroy : MonoBehaviour
+    {
+        void Awake() {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
