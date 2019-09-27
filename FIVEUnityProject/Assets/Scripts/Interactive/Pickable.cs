2c7beb1aa0e86b32e75711ab2a83c084b8d29551
Gen <xgbarry@gmail.com>
2019-09-26 22:06:40 -0400

    Added cursor prototype.

---
+++ FIVEUnityProject/Assets/Scripts/Interactive/Pickable.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace FIVE.Interactive
+{
+    internal class Pickable : MonoBehaviour
+    {
+    }
+}
---
+++ FIVEUnityProject/Assets/Scripts/Interactive/RobotCursor.cs
@@ -0,0 +1,31 @@
+﻿using FIVE.CameraSystem;
+using UnityEngine;
+
+namespace FIVE.Interactive
+{
+    internal class RobotCursor : MonoBehaviour
+    {
+        private Texture2D cursorTexture;
+        void Awake()
+        {
+            cursorTexture = Resources.Load<Texture2D>("Textures/UI/Cursor");
+            Cursor.visible = true;
+            Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
+        }
+
+        void Update()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Transform objectHit = hit.transform;
+                Pickable p = objectHit.gameObject.GetComponent<Pickable>();
+                if (Input.GetMouseButtonDown(1))
+                {
+                    Debug.Log(objectHit.gameObject);
+                    GameObject.Destroy(objectHit.gameObject);
+                }
+            }
+        }
+    }
+}
