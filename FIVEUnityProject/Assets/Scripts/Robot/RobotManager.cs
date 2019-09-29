2a32008fdbcc42f56c84ee51f4364d47d71a68c8
Wally Yang <wallyyang28@gmail.com>
2019-09-29 19:49:08 -0400

    feat: added list for robots in robot manager

--- FIVEUnityProject/Assets/Scripts/Robot/RobotManager.cs
+++ FIVEUnityProject/Assets/Scripts/Robot/RobotManager.cs
@@ -1,19 +1,26 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace FIVE.Robot
 {
     public class RobotManager : MonoBehaviour
     {
+        public List<GameObject> Robots;
+
         [SerializeField] private GameObject RobotPrefab = null;
         private static RobotManager instance;
+
         private void Awake()
         {
             instance = this;
+            Robots = new List<GameObject>();
         }
 
         public static GameObject CreateRobot()
         {
-            return Instantiate(instance.RobotPrefab, new Vector3(0f, 20f, 0f), Quaternion.identity);
+            GameObject robot = Instantiate(instance.RobotPrefab, new Vector3(0f, 20f, 0f), Quaternion.identity);
+            instance.Robots.Add(robot);
+            return robot;
         }
 
         public static GameObject GetPrefab()
