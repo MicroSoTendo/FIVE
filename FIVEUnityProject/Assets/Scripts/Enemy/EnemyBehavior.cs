7ece73ba5a9ec7a2d67956ceb30fceefd57e2485
Wally Yang <wallyyang28@gmail.com>
2019-09-29 20:01:24 -0400

    feat: added enemy behavior script

---
+++ FIVEUnityProject/Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float VisionRange;
+
+    void Start()
+    {
+        VisionRange = 10.0f;
+    }
+
+    void Update()
+    {
+        
+    }
+}
