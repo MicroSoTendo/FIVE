f2c50b5646ea79e6b70e182fc022f399e05d8e65
Wally Yang <wallyyang28@gmail.com>
2019-10-02 15:32:26 -0400

    feat: added spawn for enemies

--- FIVEUnityProject/Assets/Scripts/Enemy/EnemyManager.cs
+++ FIVEUnityProject/Assets/Scripts/Enemy/EnemyManager.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    private GameObject prefab;
-    private HashSet<GameObject> enemies;
+    public GameObject Prefab;
 
-    void Start()
+    private static EnemyManager instance;
+
+    private Vector3 spwanLocation;
+
+    private HashSet<GameObject> enemies;
+    private float elapsedTime;
+
+    private void Awake()
     {
-        
+        instance = this;
+        spwanLocation = new Vector3(10, 0, 0);
+
+        enemies = new HashSet<GameObject>();
+        elapsedTime = 0;
     }
 
     void Update()
     {
-        
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= 5.0f && enemies.Count < 10)
+        {
+            AddEnemy(spwanLocation);
+            elapsedTime = 0;
+        }
+    }
+
+    public static EnemyManager Instance()
+    {
+        return instance;
     }
 
     private void AddEnemy(Vector3 positition)
     {
-        GameObject enemy = Instantiate(prefab, positition, Quaternion.identity);
+        GameObject enemy = Instantiate(Prefab, positition, Quaternion.identity);
+        enemies.Add(enemy);
     }
 }
