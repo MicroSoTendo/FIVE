fcc370c8a73b695e857217596fc17f76d8f4415c
Yu Wei <winly8866@gmail.com>
2019-10-02 03:39:01 -0400

    Add NPC onClick Action

---
+++ FIVEUnityProject/Assets/Scripts/Interactive/NPC.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FIVE.Interactive
+{
+    public class NPC : MonoBehaviour
+    {
+        private Transform image;
+        Vector3 originalScale;
+        private bool onClick;
+        private void Start()
+        {
+            onClick = false;
+            image = gameObject.transform.Find("Canvas").Find("Image");
+            originalScale = image.localScale;
+        }
+        IEnumerator ChangeOverTime(float time)
+        {
+            
+            Vector3 destinationScale = new Vector3(1.05f, 1.05f, 1.05f);
+
+            float currentTime = 0.0f;
+
+            do
+            {
+                image.localScale = Vector3.Lerp(originalScale, destinationScale, currentTime / time);
+                currentTime += Time.deltaTime;
+                yield return null;
+            } while (currentTime <= time);
+
+        }
+
+
+        public void OnMouseOver()
+        {
+            onClick = true; onClick = true;
+            StartCoroutine(ChangeOverTime(0.2f));
+        }
+        public void  OnMouseExit()
+        {
+            onClick = false;
+            
+        }
+        public void OnMouseDown()
+        {
+            Debug.Log("ClickedNPC");
+        }
+        public void Update()
+        {
+            if (!onClick)
+            {
+                image.localScale = originalScale;
+            }
+        }
+    }
+}
