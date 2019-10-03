fd29c00848a5e0c81f89ea15e34d923480715039
Gen <xgbarry@gmail.com>
2019-10-03 14:59:35 -0400

    Case change.

---
+++ FIVEUnityProject/Assets/Scripts/RobotComponent/CPU.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace FIVE.RobotComponent
+{
+    public class CPU : MonoBehaviour
+    {
+        public int Speed; // instruction executed per frame
+        public float PowerConsumption; // 0.0f ~ 1.0f;
+
+        private void Start()
+        {
+            Speed = 1;
+            PowerConsumption = 1.0f;
+        }
+    }
+}
