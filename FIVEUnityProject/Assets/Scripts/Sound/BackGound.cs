9c5ced0e58bebcb7ac72f7a91e03e3c49517a14b
Gen <xgbarry@gmail.com>
2019-10-13 21:53:47 -0400

    Moved sound into independent namespace.

---
+++ FIVEUnityProject/Assets/Scripts/Sound/BackGound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace FIVE.Sound
+{
+    public class Backgound : MonoBehaviour
+    {
+        public AudioClip audios;
+        private AudioSource auz;
+
+        private void Start()
+        {
+            auz = GetComponent<AudioSource>();
+            gameObject.GetComponent<AudioSource>().clip = audios;
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+
+        private void Update()
+        {
+        }
+
+        private void ExecuteScript()
+        {
+        }
+    }
+}
