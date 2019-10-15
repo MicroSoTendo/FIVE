4b3f69d2038918e47bfd441595583727c245d331
Gen <xgbarry@gmail.com>
2019-10-15 16:06:40 -0400

    Removed redundant stuff.

--- ListServer/src/main.rs
+++ ListServer/src/main.rs
@@ -2,14 +2,7 @@ use std::net::{TcpListener, TcpStream};
 use std::io::Read;
 use std::collections::HashMap;
 use std::vec::Vec;
-// var writer = new BinaryWriter(new MemoryStream());
-// // create message
-// writer.Write((ushort)NetworkServer.connections.Count);
-// writer.Write((ushort)NetworkManager.singleton.maxConnections);
-// byte[] titleBytes = Encoding.UTF8.GetBytes(gameServerTitle);
-// writer.Write((ushort)titleBytes.Length);
-// writer.Write(titleBytes);
-// writer.Flush();
+
 #[repr(packed)]
 struct RoomInfo {
     connections_count: u16,
@@ -26,11 +19,11 @@ unsafe fn handle_client(mut stream: TcpStream, clients_map : &mut HashMap<u128, 
                 let mut room_info_raw = Vec::new();
                 match stream.read(&mut room_info_raw) {
                     Ok(_) => {
-                        let room_info : RoomInfo = unsafe { std::ptr::read(room_info_raw.as_ptr() as * const _)};
+                        let room_info : RoomInfo = { std::ptr::read(room_info_raw.as_ptr() as * const _)};
                         let mut guid_raw = [0 as u8; 8];
                         match stream.read(&mut guid_raw) {
                             Ok(_) => {
-                                let guid : u128= unsafe { std::ptr::read(guid_raw.as_ptr() as * const _)};
+                                let guid : u128 =  { std::ptr::read(guid_raw.as_ptr() as * const _)};
                                 clients_map.insert(guid, room_info);
                             }
                             _ => {}
@@ -50,7 +43,7 @@ fn main() {
     println!("Server listening on port 8887");
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => unsafe {
+            Ok(stream) => unsafe {
                 handle_client(stream, &mut clients_map);
             }
             _ => {}
