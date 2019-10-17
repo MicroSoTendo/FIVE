84241fb9c5e70800bc8a7185cdc5e136432b24ed
Yu Wei <winly8866@gmail.com>
2019-10-17 16:51:07 -0400

    NPCprefab& initialized

---
+++ FIVEUnityProject/Assets/Scripts/Interactive/NPCInit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInit : MonoBehaviour
+{
+    public GameObject NPC;
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
