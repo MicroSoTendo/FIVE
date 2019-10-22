08f0919df9136c8cc3af2aa0c864fa05b9596788
Wally Yang <wallyyang28@gmail.com>
2019-10-22 15:16:38 -0400

    feat: added patrol for alien beetles

--- FIVEUnityProject/Assets/Scripts/Enemy/EnemyBehavior.cs
+++ FIVEUnityProject/Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -12,35 +12,57 @@ namespace FIVE
 
         private GameObject currTarget;
 
+        private float patrolDirection;
+
+        private float visionRange;
+
         private float speed;
-        public float VisionRange;
+        private float elapsedTime;
 
         private void Start()
         {
-            VisionRange = 10.0f;
+            visionRange = 10.0f;
 
             cc = GetComponent<CharacterController>();
             animator = GetComponent<Animator>();
 
+            patrolDirection = Random.Range(0, 360);
+            transform.Rotate(0, patrolDirection, 0);
+
             speed = 10.0f;
+            elapsedTime = 0;
         }
 
         private void Update()
         {
-            if (currTarget == null ||
-                Vector3.Distance(transform.position, currTarget.transform.position) > 2 * VisionRange)
+            if (cc.velocity.magnitude > 0)
+            {
+                animator.SetTrigger("walk");
+            }
+            else
             {
                 animator.SetTrigger("idle2");
+            }
+
+            if (currTarget == null)
+            {
+                Patrol();
                 SearchTarget();
             }
             else
             {
-                animator.SetTrigger("walk");
-                Vector3 distance = currTarget.transform.position - transform.position;
-                transform.forward = distance;
-                if (distance.magnitude > 5.0f)
+                if (Vector3.Distance(transform.position, currTarget.transform.position) > 2 * visionRange)
                 {
-                    cc.SimpleMove(Vector3.Normalize(distance) * speed);
+                    currTarget = null;
+                }
+                else
+                {
+                    Vector3 distance = currTarget.transform.position - transform.position;
+                    transform.forward = distance;
+                    if (distance.magnitude > 5.0f)
+                    {
+                        cc.SimpleMove(Vector3.Normalize(distance) * speed);
+                    }
                 }
             }
         }
@@ -50,7 +72,7 @@ namespace FIVE
             foreach (GameObject robot in RobotManager.Instance.Robots)
             {
                 if (Physics.SphereCast(transform.position, 3.0f, robot.transform.position - transform.position,
-                    out RaycastHit hitInfo, VisionRange))
+                    out RaycastHit hitInfo, visionRange))
                 {
                     currTarget = robot;
                 }
@@ -59,6 +81,17 @@ namespace FIVE
 
         private void Patrol()
         {
+            elapsedTime += Time.deltaTime;
+            if (elapsedTime > 3)
+            {
+                patrolDirection = Random.Range(0, 90);
+                transform.Rotate(0, patrolDirection, 0);
+                elapsedTime = 0;
+            }
+            else
+            {
+                cc.SimpleMove(transform.forward * speed);
+            }
         }
     }
 }
