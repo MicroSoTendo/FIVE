e87ae947cb05c6dc1d9b5f08d55008618e40542a
Gen <xgbarry@gmail.com>
2019-10-24 02:49:38 -0400

    Update multiplayer init logic.

--- FIVEUnityProject/Assets/Scripts/GameModes/Multiplayers.cs
+++ FIVEUnityProject/Assets/Scripts/GameModes/Multiplayers.cs
@@ -1,4 +1,5 @@
-﻿using FIVE.EventSystem;
+﻿using System;
+using FIVE.EventSystem;
 using FIVE.Network;
 using FIVE.UI;
 using FIVE.UI.Multiplayers;
@@ -10,6 +11,7 @@ using FIVE.TerrainSystem;
 using FIVE.UI.Background;
 using FIVE.UI.CodeEditor;
 using FIVE.UI.InGameDisplay;
+using FIVE.UI.NPC;
 using UnityEngine;
 
 
@@ -24,32 +26,37 @@ namespace FIVE.GameModes
             lobbyWindow = UIManager.Create<LobbyWindowViewModel>();
             lobbyWindow.IsActive = true;
             EventManager.Subscribe<OnCreateRoomRequested, CreateRoomRequestedEventArgs>(CreateRoomHandler);
+            EventManager.Subscribe<OnJoinRoomRequested>(JoinRoomHandler);
         }
 
-        private IEnumerator InitHost()
+        private IEnumerator CommonInitRoutine()
         {
-            UIManager.Get<BackgroundViewModel>().IsActive = false;
             TerrainManager.CreateTerrain(Vector3.zero);
+            CameraManager.Remove("GUI Camera"); 
+            UIManager.Create<HUDViewModel>().IsActive = true;
+            UIManager.Create<NPCDialogueViewModel>().IsActive = false;
+            UIManager.Create<InGameMenuViewModel>().IsActive = false;
             yield return null;
+        }
+        private IEnumerator ClientInitRoutine()
+        {
+            
+            yield return null;
+        }
+        private IEnumerator HostInitRoutine()
+        {
             CameraManager.AddCamera("DefaultCamera-1", new Vector3(-40, 115, -138), Quaternion.Euler(30, 10, 0));
             CameraManager.AddCamera("DefaultCamera-2", new Vector3(33, 70.5f, -49), Quaternion.Euler(50, -32, 0));
             CameraManager.AddCamera("DefaultCamera-3", new Vector3(36.5f, 180f, 138), Quaternion.Euler(63, 230, -5f));
-            CameraManager.Remove("GUI Camera");
-            GameObject NPC = Resources.Load<GameObject>("EntityPrefabs/NPC/NPCsinglePrefabs/NPCBlue");
-            yield return null;
-            Instantiate(NPC, new Vector3(-2.91f, 0f, 3.96f), Quaternion.Euler(new Vector3(0f, 140f, 0)));
-            Instantiate(NPC, new Vector3(3.55f, 0f, -4.11f), Quaternion.Euler(new Vector3(0f, 270f, 0)));
-            Instantiate(NPC, new Vector3(3.64f, 0.22f, 12.76f), Quaternion.Euler(new Vector3(0f, 180f, 0)));
-            Instantiate(NPC, new Vector3(-2.91f, 0f, 3.96f), Quaternion.Euler(new Vector3(0f, 140f, 0)));
-            Instantiate(NPC, new Vector3(3f, 0f, 3.96f), Quaternion.Euler(new Vector3(0f, 270f, 0)));
-            Instantiate(NPC, new Vector3(-3f, 0f, 13f), Quaternion.Euler(new Vector3(0f, 140f, 0)));
-            yield return null;
-            UIManager.Create<HUDViewModel>().IsActive = true;
-            UIManager.Create<InGameMenuViewModel>().IsActive = false;
+            NPCInit.Initialize();
+           
             //TODO: Parametrize prefab name with a UI selector
             GameObject robot = RobotManager.CreateRobot("robotSphere", new Vector3(0, 20, 0), Quaternion.identity);
+            RobotManager.ActiveRobot = robot;
+            GameObject robot1 = RobotManager.CreateRobot("robotSphere", new Vector3(0, 20, 20), Quaternion.identity);
+            GameObject enemyManagerPrefab = Resources.Load<GameObject>("InfrastructurePrefabs/EnemyManager");
+            Instantiate(enemyManagerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
             Inventory inventory = InventoryManager.AddInventory(robot);
-            yield return null;
             InventoryViewModel inventoryViewModel = UIManager.Create<InventoryViewModel>();
             CodeEditorViewModel codeEditorViewModel = UIManager.Create<CodeEditorViewModel>();
             ItemDialogViewModel itemDialogViewModel = UIManager.Create<ItemDialogViewModel>();
@@ -57,14 +64,22 @@ namespace FIVE.GameModes
             inventoryViewModel.IsActive = false;
             codeEditorViewModel.IsActive = false;
             itemDialogViewModel.IsActive = false;
-            yield return null;
             CameraManager.SetCameraWall();
+            yield return null;
         }
+
         private void CreateRoomHandler(object sender, CreateRoomRequestedEventArgs e)
         {
             NetworkManager.CreateRoom(e.Name, e.MaxPlayer, e.HasPassword, e.Password);
-            StartCoroutine(InitHost());
+            StartCoroutine(HostInitRoutine());
             lobbyWindow.IsActive = false;
         }
+        
+        private void JoinRoomHandler(object sender, EventArgs e)
+        {
+            
+        }
+
+
     }
 }
