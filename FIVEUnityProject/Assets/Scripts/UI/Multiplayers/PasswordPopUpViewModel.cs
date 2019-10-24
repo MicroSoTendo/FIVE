e9cc8d2864fac800fa072b2845ed1bd7bb283a04
Gen <xgbarry@gmail.com>
2019-10-24 02:48:25 -0400

    Added password VM.

---
+++ FIVEUnityProject/Assets/Scripts/UI/Multiplayers/PasswordPopUpViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine.UI;
+
+namespace FIVE.UI.Multiplayers
+{
+    class PasswordPopUpViewModel : ViewModel
+    {
+        protected override string PrefabPath { get; } = "EntityPrefabs/UI/Multiplayers/PasswordPopUp";
+        public Button ConfirmButton { get; }
+        public TMP_InputField PasswordInputField { get; }
+        
+        public PasswordPopUpViewModel()
+        {
+            ZIndex = 2;
+            ConfirmButton = Get<Button>(nameof(ConfirmButton));
+            PasswordInputField = Get<TMP_InputField>(nameof(PasswordInputField));
+        }
+    }
+}
