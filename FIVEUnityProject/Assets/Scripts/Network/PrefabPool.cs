0df704dd57777c46d37174721d0c207c7e2fc9a2
Gen <xgbarry@gmail.com>
2019-10-30 01:07:51 -0400

    Support for get prefab ID by gameobject instance.

--- FIVEUnityProject/Assets/Scripts/Network/PrefabPool.cs
+++ FIVEUnityProject/Assets/Scripts/Network/PrefabPool.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace FIVE.Network
 {
@@ -6,16 +7,26 @@ namespace FIVE.Network
     {
         public static PrefabPool Instance { get; } = new PrefabPool();
         public GameObject this[int i] => prefabDatabase[i];
-        public int this[GameObject prefab] => prefabDatabase[prefab];
-        private BijectMap<int, GameObject> prefabDatabase;
+        public int this[GameObject gameObjectInstance] => instancePrefabID[gameObjectInstance];
+        private readonly Dictionary<int, GameObject> prefabDatabase;
+        private readonly Dictionary<GameObject, int> instancePrefabID;
         private PrefabPool()
         {
+            prefabDatabase = new Dictionary<int, GameObject>();
+            instancePrefabID = new Dictionary<GameObject, int>();
             int id = 0;
             GameObject[] prefabs = Resources.LoadAll<GameObject>("EntityPrefabs/");
             foreach (GameObject prefab in prefabs)
             {
                 prefabDatabase.Add(id++, prefab);
             }
+        }
+
+        public GameObject Instantiate(int id)
+        {
+            var go = GameObject.Instantiate(prefabDatabase[id]);
+            instancePrefabID.Add(go, id);
+            return go;
         }
     }
 }
