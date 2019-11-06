c31116a7b43e941ee927de8b7799d3587f451508
Yu Wei <winly8866@gmail.com>
2019-11-06 00:13:22 -0500

    Blacksmith add result

--- FIVEUnityProject/Assets/Scripts/Interactive/Blacksmith/ComponentsRead.cs
+++ FIVEUnityProject/Assets/Scripts/Interactive/Blacksmith/ComponentsRead.cs
@@ -4,25 +4,71 @@ using UnityEngine;
 
 namespace FIVE.Interactive.BlackSmith
 {
-    public class ComponentsRead : MonoBehaviour
+    public class ComponentsRead
     {
-        private TextAsset csvFile;
-        private string[] records;
-        private string[] currentTerm;
-        void Start()
+        private static TextAsset csvFile;
+        private static string[] records;
+        private static string[] currentTerm;
+        private static List<List<string>> CompositeLists;
+        public static List<string> PathLists;
+        public static int index;
+        private static void GenerateLists()
         {
+            index = 0;
             csvFile = Resources.Load<TextAsset>("CSV/Composite");
             records = csvFile.text.Split('\n');
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            for(int i = 0; i < records.Length; i++)
+            CompositeLists = new List<List<string>>(records.Length);
+            PathLists = new List<string>();
+            for (int i = 0; i < records.Length; i++)
             {
                 string[] fields = records[i].Split(',');
+                List<string> temp = new List<string> { fields[0], fields[1], fields[2] };
+                CompositeLists.Add(temp);
+                PathLists.Add(fields[3]);
+
+            }
+        }
+        private static bool isCompositeComponents(List<GameObject> compositeItems)
+        {
+            bool isComp = true;
+            GenerateLists();
+            SortedSet<string> sortedCompositeItems = new SortedSet<string>();
+            foreach (GameObject a in compositeItems)
+            {
+                sortedCompositeItems.Add(a.name);
+            }
+            List<string> sortedCompositeItemList = new List<string>(sortedCompositeItems);
+
+            for (int i = 0; i < CompositeLists.Count; i++)
+            {
+                SortedSet<string> set = new SortedSet<string>(CompositeLists[i]);
+                List<string> sorteSet = new List<string>(set);
+                isComp = true;
+                for(int a = 0; a < sorteSet.Count; a++)
+                {
+                    if (!sortedCompositeItemList[a].Contains(sorteSet[a]))
+                    {
+                        isComp = false;
+                    }
+                }
+                if(isComp == true)
+                {
+                    index = i;
+                    break;
+                }
                 
             }
+                
+            return isComp;
+        }
+        public static GameObject generateItem(List<GameObject> compositeItems)
+        {
+            if (isCompositeComponents(compositeItems))
+            {
+                return Resources.Load<GameObject>(PathLists[index]);
+            }
+            
+            return null;
         }
     }
 }
