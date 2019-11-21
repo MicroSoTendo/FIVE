d94e904151409d481def54e0221e2ebd13b5fa6f
Laurence <liuxy6@gmail.com>
2019-11-21 03:37:59 +0000

    Add camera shake

--- FIVEUnityProject/Assets/Scripts/Robot/RobotSphere/RobotSphere.cs
+++ FIVEUnityProject/Assets/Scripts/Robot/RobotSphere/RobotSphere.cs
@@ -175,6 +175,7 @@ namespace FIVE.Robot
                 StartCoroutine(ShutGunfire(gunfire));
                 GameObject bullet = Instantiate(BulletPrefab, transform.position + transform.forward * 10f + new Vector3(0, 1, 0), Quaternion.identity);
                 bullet.GetComponent<Bullet>().Target = target;
+                fpsCamera.GetComponent<CameraShake>().ShakeCamera(0.5f, 0.5f);
             }
         }
 
@@ -188,6 +189,7 @@ namespace FIVE.Robot
                 GameObject bullet = Instantiate(BulletPrefab, transform.position + transform.forward * 10f + new Vector3(0, 1, 0), Quaternion.identity);
                 bullet.GetComponent<Bullet>().Target = target.transform.position;
                 StartCoroutine(KillAlien(target));
+                fpsCamera.GetComponent<CameraShake>().ShakeCamera(0.5f, 0.5f);
             }
         }
 
