c9985f9cd6122c2a2ba4f7d99b46ebcb821e0504
Laurence <liuxy6@gmail.com>
2019-12-01 19:35:50 -0500

    FIx

--- FIVEUnityProject/Assets/Scripts/Robot/RobotSphere/RobotSphere.cs
+++ FIVEUnityProject/Assets/Scripts/Robot/RobotSphere/RobotSphere.cs
@@ -178,10 +178,13 @@ namespace FIVE.Robot
         private IEnumerator KillAlien(GameObject alien)
         {
             yield return new WaitForSeconds(0.2f);
-            EnemyBehavior enemyBehavior = alien.GetComponent<EnemyBehavior>();
-            if (enemyBehavior != null)
+            if (alien != null)
             {
-                enemyBehavior.OnHit();
+                EnemyBehavior enemyBehavior = alien.GetComponent<EnemyBehavior>();
+                if (enemyBehavior != null)
+                {
+                    enemyBehavior.OnHit();
+                }
             }
         }
 
