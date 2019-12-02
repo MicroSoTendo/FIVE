c85ecd29e3ea22021801b5b8f495eee11d6c96f9
Yu Wei <winly8866@gmail.com>
2019-12-01 20:52:34 -0500

    HUD fix text and climate add

---
+++ FIVEUnityProject/Assets/Scripts/Interactive/ClimateManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FIVE.Interactive
+{
+    public class ClimateManager : MonoBehaviour
+    {
+
+        // Update is called once per frame
+        void Update()
+        {
+            //EnviroWeatherPreset a = 
+        }
+    }
+}
+
